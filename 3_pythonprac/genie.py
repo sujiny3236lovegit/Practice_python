58b69e139ea21144d57d0e2836c8d5a93b16f6ee
sujiny3236lovegit <69749672+sujiny3236lovegit@users.noreply.github.com>
2021-01-29 13:25:44 +0900

    python

--- 3_pythonprac/genie.py
+++ 3_pythonprac/genie.py
@@ -1,28 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-db = client.dbsparta
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303', headers=headers)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20210127', headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-trs = soup.select('#old_content > table > tbody > tr')
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
 for tr in trs:
-    a_tag = tr.select_one('td.title > div > a')
-    if a_tag is not None:
-        rank = tr.select_one('td:nth-child(1) > img')['alt']
-        title = a_tag.text
-        star = tr.select_one('td.point').text
-        doc = {
-            'rank': rank,
-            'title': title,
-            'star': star
-        }
-        db.movies.insert_one(doc)
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    rank = tr.select_one('td.number').text[0:2].strip()
+    artist = tr.select_one('td.info > a.artist.ellipsis').text
+    print(rank, title, '-' ,artist)
