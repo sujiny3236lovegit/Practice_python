9bed7088f92fccdb13ff3a9ba7e13464dd660b05
sujiny3236lovegit <69749672+sujiny3236lovegit@users.noreply.github.com>
2021-02-01 13:52:40 +0900

    main에서 커밋

--- 2_projects/alonememo/app.py
+++ 2_projects/alonememo/app.py
@@ -15,16 +15,36 @@ def home():
 
 @app.route('/memo', methods=['GET'])
 def listing():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg':'GET 연결되었습니다!'})
+    articles = list(db.articles.find({}, {'_id': False}))
+    return jsonify({'':articles})
 
 ## API 역할을 하는 부분
 @app.route('/memo', methods=['POST'])
 def saving():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg':'POST 연결되었습니다!'})
+    url_receive = request.form['url_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+
+    doc = {
+        'title':title,
+        'image':image,
+        'desc':desc,
+        'url':url_receive,
+        'comment':comment_receive
+    }
+
+    db.articles.insert_one(doc)
+
+    return jsonify({'msg':'저장이 완료되었습니다!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
